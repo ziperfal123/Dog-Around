86485c586dd53908af3d155114f262f1b669805d
Yaniv Ziperfal <ziperfal123@gmail.com>
2019-07-01 21:22:30 +0300

    final changes

--- src/components/RunInfoNumeric.js
+++ src/components/RunInfoNumeric.js
@@ -2,11 +2,7 @@ import RunInfo from './RunInfo'
 import PropTypes from 'prop-types'
 export default class RunInfoNumeric extends RunInfo {
   formatValue = () => {
-    let info
-    if (this.state.value < 0) info = 0
-    else info = this.state.value
-
-    return [info.toFixed(2), this.props.unit].join(' ')
+    return [this.state.value.toFixed(2), this.props.unit].join(' ')
   }
 }
 
